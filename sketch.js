78b900fced3e1895a7ed70c5fc46e1ce2c215f80
daniel-gibbens <dr.gibbens@outlook.com>
2023-10-11 22:30:31 +1300

    Refactored and commented

--- sketch.js
+++ sketch.js
@@ -3,22 +3,22 @@ let maskImg=null;
 let ringMaskImg=null;
 let renderCounter=0;
 
-// change these three lines as appropiate
 let sourceFile = "eyes/hazel-eye.jpg";
 let maskFile   = "eyes/iris-masks/hazel-eye-iris-mask.jpg";
 let pupilMask   = "eyes/pupil-masks/hazel-eye-pupil-mask.jpg";
-//let sourceFile = "eyes/face.jpg";
-//let maskFile   = "eyes/iris-masks/face-mask.png";
 
-let outputFile = "Ringmask.png";
+let outputFile = "hazel_training.png";
 
+// Used for storing pixels that contain the iris
 let eyePixels = []
+
+// 2D array treated as ringmask image
+let ringMask = [];
 
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
   pupilMaskImg = loadImage(pupilMask);
-
 }
 
 function setup () {
@@ -34,6 +34,7 @@ function setup () {
 
 }
 
+// Blurs a pixel by using the average pixel colour around it
 function blurPixel(x, y, radius) {
   let r = 0;
   let g = 0;
@@ -58,17 +59,15 @@ function blurPixel(x, y, radius) {
       }
     }
   }
-
-  // Calculate the average color in the neighborhood
+  // Calculate the average colour in the neighborhood
   r /= count;
   g /= count;
   b /= count;
 
-  return [r, g, b];
+  return [r, g, b, 255];
 }
 
-ringMask = [];
-
+// Creates the Ring mask from a pupil mask
 function makeRingMask() {
   for (let x = 0; x < sourceImg.width; x++) {
     let col = [];
@@ -77,6 +76,7 @@ function makeRingMask() {
       let pupil = pupilMaskImg.get(x, y);
       strokeWeight(0)
 
+      // Only blur the white pixels
       const IS_WHITE = pupil[0] === 255 && pupil[1] === 255 && pupil[2] === 255;
       if (IS_WHITE) {
         pupil = blurPixel(x, y, 5);
@@ -85,11 +85,14 @@ function makeRingMask() {
     }
     ringMask.push(col)
   }
+
+  // After blurring set pixels to white or black
   for (let x = sourceImg.width-1; x >= 0; x--) {
     for (let y = 0; y < sourceImg.height; y++) {
 
       let pupil = ringMask[x][y];
 
+      // Set all pure white pixels to be black to cut out the middle
       const IS_WHITE = pupil[0] === 255 && pupil[1] === 255 && pupil[2] === 255;
       if (IS_WHITE) {
         pupil = [0,0,0,255]
@@ -102,6 +105,7 @@ function makeRingMask() {
 }
 
 function draw () {
+  // Creates the ring mask
   makeRingMask();
 
   for (let x = sourceImg.width; x > 0; x--) {
@@ -109,33 +113,37 @@ function draw () {
       let pix = sourceImg.get(x, y);
       let mask = maskImg.get(x, y);
       let pupil = ringMask[x-1][y];
-      stroke(pix)
       strokeWeight(0)
       customPixel(pix, mask, x, y, pupil)
     }
   }
+  // Flip the eyePixels to draw the iris in the other direction
   eyePixels.reverse();
   eyePixels.forEach(eyePix => {
     pix = eyePix.pix
+    // Make white pixel ring around the pupil using the ringmask
     if (eyePix.ringMask[0] > 128) {
       stroke(255,255,255);
     } else {
+      // Draw the iris pixels
       if (eyePix.y % 10 == 1) {
+        // Main pixels
         strokeWeight(7)
         stroke(pix[2],pix[1],pix[0])
-        
       } else {
+        // Fill the gaps
         strokeWeight(1)
         stroke(pix[1], pix[2], pix[0])
       }
     }
     
-    // reflective part in the eye
+    // Reflective part in the eye
     if (pix[0] > 200 && pix[1] > 200 && pix[2] > 200) {
       stroke(pix[2] * 7/8,pix[1] * 9/10,pix[0])
     }
     point(eyePix.x,eyePix.y)
   });
+  
   renderCounter = renderCounter + 1;
   if(renderCounter > 1) {
     console.log("Done!")
@@ -147,6 +155,7 @@ function draw () {
 
 function customPixel(pix, maskGiven, x, y, pupil) {
   if(maskGiven[0] > 128) {
+    // Store the eye pixels for swapping draw order
       eyePixels.push({
         "pix" : pix,
         "maskGiven" : maskGiven,
@@ -155,26 +164,33 @@ function customPixel(pix, maskGiven, x, y, pupil) {
         "ringMask" : pupil
       })
   } else {
-    h = 1
+    // Draw the background pixels
+    thickness = 1
+    // Increased red channel on brighter areas
     if (pix[0] > 80 && pix[1] > 80 && pix[1] > 80) {
       if (y % 9 == 1) {
-        h =7.2
+        // Main pixels
+        thickness = 7.2
         fill(pix[0]*1.2-80,pix[1]/2-40,pix[2]/2-60)
       } else {
-        h=1
+        // Fill the gaps
+        thickness = 1
         fill(pix[0]/2 + 20-40,pix[1]/2-40,pix[2]/2-60)
       }  
     } else {
+      // Darker pixels
       if (y % 9 == 1) {
-        h=7.2
+        // Main pixels
+        thickness = 7.2
         fill(pix[0]-45,pix[1]-55,pix[2]-55)
       } else {
-        h=1
+        // Fill the gaps
+        thickness = 1
         fill(pix[0]-45,pix[1]-25,pix[2]-55)
       } 
     }
     noStroke()
-    rect(x,y,h)  
+    rect(x,y,thickness)  
   }
 }
 
